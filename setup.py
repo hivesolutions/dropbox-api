5c8b87dbbf68d7df6d4b47663c038b7f6843fa67
joamag <joamag@gmail.com>
2017-05-16 12:54:24 +0100

    new setup file and link

--- setup.py
+++ setup.py
@@ -48,7 +48,7 @@ setuptools.setup(
     description = "Dropbox API Client",
     license = "Apache License, Version 2.0",
     keywords = "dropbox api",
-    url = "http://dropbox_api.hive.pt",
+    url = "http://dropbox-api.hive.pt",
     zip_safe = False,
     packages = [
         "dropbox"
